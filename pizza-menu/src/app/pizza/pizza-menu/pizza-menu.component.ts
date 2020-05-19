40ef73ffc7a432cf81872173253722d2db1dcbb8
Alex omaset <alexomaset711@gmail.com>
2020-05-19 12:53:49 +0300

    initialized mat application

---
+++ pizza-menu/src/app/pizza/pizza-menu/pizza-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pizza-menu',
+  templateUrl: './pizza-menu.component.html',
+  styleUrls: ['./pizza-menu.component.scss']
+})
+export class PizzaMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
